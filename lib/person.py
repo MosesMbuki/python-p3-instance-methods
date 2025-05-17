d81d1cfa5c396e9c5aab2e8a1513066ecd3723e7
+ <moses.mutitu@student.moringaschool.com>
2025-05-17 14:15:12 +0300

    done

--- lib/person.py
+++ lib/person.py
@@ -2,6 +2,11 @@
 
 class Person:
     # Class body goes here
+    person = "guido"
+    def talk(self):
+        print("Hello World!")
 
+    def walk(self):
+        print("The person is walking.")
     #Instance method definition
     pass
